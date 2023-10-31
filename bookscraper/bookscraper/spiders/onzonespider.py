22a6d7a04b076f2ca6f087692da1ed822bcb818d
Veselin Delchev <delchev.v5454@gmail.com>
2023-10-31 14:47:23 +0200

    Updated Ozone crawler
    now scrape the information after opening each phone as new tab and search for it's data.

--- bookscraper/bookscraper/spiders/onzonespider.py
+++ bookscraper/bookscraper/spiders/onzonespider.py
@@ -9,11 +9,26 @@ class OnzonespiderSpider(scrapy.Spider):
     def parse(self, response):
         phones = response.css('div.col-xs-3')
         for phone in phones:
-            yield{
-                'model': phone.css('span.title::text').get(),
-            }
+
+            phone_url = phone.css('a.product-box').attrib['href']
+
+            yield response.follow(phone_url, callback=self.parse_book_page)
+            # yield {
+            #     'model': phone.css('span.title::text').get(),
+            #     'price': phone.css("span.regular-price span.price::text").get()
+            # }
 
         next_page = response.css('a.next').attrib['href']
-        print(next_page)
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+    def parse_book_page(self, response):
+        yield {
+            'name': response.css("div.col-xs-5 h1::text").get(),
+            'processor': response.css("ul.attribute-list > li:first-child span::text").get(),
+            'screen size': response.css("ul.attribute-list > li:nth-child(2) span::text").get(),
+            'RAM': response.css("ul.attribute-list > li:nth-child(4) span::text").get(),
+            "HDD" : response.css("ul.attribute-list > li:nth-child(5) span::text").get(),
+            'old_price': response.css("div.price-box p.old-price span.price::text").get(),
+            'special_price':  response.css("div.price-box p.special-price span.price::text").get(),
+        }
